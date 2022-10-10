4802809c6e6c904d96d2c2c9a2faaf678e2e4e05
arseil <84498832+arseil@users.noreply.github.com>
2022-10-10 22:12:23 +0300

    addition to the second section

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+/* $(document).ready(function() {
 	$('.carousel__inner').slick({
 		speed: 1200,
 		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
@@ -27,4 +27,4 @@ $(document).ready(function() {
 			}
 		]
 	});
-});
+}); */
